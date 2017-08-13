438fe318c914a985eaf749e31a1f6a53b36cda82
CoralineAda <coraline@idolhands.com>
2017-08-13 15:41:40 -0500

    Clean up the board

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,14 +4,14 @@ class Board
 
   def self.coords(position)
     case position
-    when 0..10
-      [0, (10 - position) * SQUARE_OFFSET]
-    when 11..20
-      [(position - 10) * SQUARE_OFFSET, 0]
-    when 21..30
-      [10 * SQUARE_OFFSET, (position - 20) * SQUARE_OFFSET]
-    when 31..40
-      [(40 - position) * SQUARE_OFFSET, 10 * SQUARE_OFFSET]
+    when 0..8
+      [position * SQUARE_OFFSET, 0]
+    when 9..16
+      [8 * SQUARE_OFFSET, (position - 8) * SQUARE_OFFSET]
+    when 17..24
+      [(24 - position) * SQUARE_OFFSET, 8 * SQUARE_OFFSET]
+    when 25..32
+      [0, (32 - position) * SQUARE_OFFSET]
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,16 @@
 Space.delete_all
 
-(1..40).to_a.each do |position|
+(0..31).to_a.each do |position|
   name = case position
-  when 1; "The Bottoms"
-  when 5; "Rebel Safehouse"
-  when 10; "Barton's Forge"
-  when 15; "The Stables"
-  when 20; "The Trifle"
-  when 25; "The Broken Pestle"
-  when 25; "Market Square"
-  when 30; "Temple of Pelor"
-  when 35; "Guild Hall"
-  when 40; "Sewer Entrance"
+  when 0; "The Bottoms"
+  when 4; "Rebel Safehouse"
+  when 8; "Barton's Forge"
+  when 12; "The Stables"
+  when 16; "The Trifle"
+  when 20; "The Broken Pestle"
+  when 24; "Temple of Pelor"
+  when 28; "Market Square"
+  when 31; "Sewer Entrance"
   else; ""
   end
   Space.create(name: name, position: position)
